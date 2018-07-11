288381a9a3d2b8dac38db1f1040b908abe65c748
caesar <linmomo91@outlook.com>
2018-07-11 16:22:20 +0800

    Commit login-vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
+    <v-app>
+      <page-header/>
+      <main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
     <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
   </div>
 </template>
 
 <script>
+import PageHeader from "@/components/Header.vue";
+
 export default {
-  name: 'App'
-}
+  name: "app",
+  components: {
+    PageHeader
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.v-input-group--text-field input {
+  border-bottom: 1px solid black;
 }
 </style>
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,7 +3,7 @@
         <v-flex xs6 offset-xs3>
             <div class="white elevation-2" dark>
                 <v-toolbar class="cyan" dark>
-                    <v-toolbar-title >Register</v-toolbar-title>
+                    <v-toolbar-title >Login</v-toolbar-title>
                 </v-toolbar>
 
                 <div class="pl-4 pr-4 pt-2 pb-2">
@@ -22,8 +22,8 @@
                     <v-btn
                         dark
                         class="cyan"
-                        @click="register">
-                        Register
+                        @click="login">
+                        Login
                     </v-btn>
                 </div>
             </div>
@@ -42,9 +42,9 @@ export default {
     };
   },
   methods: {
-    async register() {
+    async login() {
       try {
-        await AuthenticationService.register({
+        await AuthenticationService.login({
           email: this.email,
           password: this.password
         });
